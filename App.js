5c07ee2c902c9edb9b09c47276d060505361e64b
harshitaajaria <harshitaajaria@gmail.com>
2024-09-04 00:51:38 +0530

    foundation

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
  import React from "react";
- import ReactDOM from "react-dom";
+ import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1",{id:"heading",xyz:"abc"},"hello world");
+ const number =100;
+
+ const Title=()=>
+ {
+    return(
+        <h1>hii title</h1>
+    )
+ }
+const heading =  <h1> this is jsx </h1>
+
+const Headingcom = ()=>(
+  
+        <div>
+           <Title/>
+           {number},
+           <h1>component </h1>
+        </div>
+)
+
+
 console.log(heading);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Headingcom/>);
